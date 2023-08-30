c23247975135658cfd83104a399b310bee2a7c8f
daheejo <happyj6528@gmail.com>
2023-08-30 22:52:18 +0900

    chore: box 컴포넌트 수정

--- src/components/MarkerInfoBox.tsx
+++ src/components/MarkerInfoBox.tsx
@@ -1,17 +1,21 @@
+import React from "react";
 interface markerInfoBoxProps {
-    name: string;
+  info: {
+    name?: string;
     lat: number;
     lng: number;
+  };
 }
 
-const MarkerInfoBox: React.FC<markerInfoBoxProps> = ({ name, lat, lng }) => {
-    return (
-        <div className="infobox">
-            <p className="info-text">{name}</p>
-            <p className="info-text">lat: {lat}</p>
-            <p className="info-text">lng: {lng}</p>
-        </div>
-    )
-}
+const MarkerInfoBox: React.FC<markerInfoBoxProps> = ({ info }) => {
+
+  return (
+    <div className="infobox">
+      <p className="info-text">{info?.name}</p>
+      <p className="info-text">lat: {info?.lat}</p>
+      <p className="info-text">lng: {info?.lng}</p>
+    </div>
+  );
+};
 
 export default MarkerInfoBox;
