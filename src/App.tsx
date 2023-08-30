f591a384c5b68249576ea288e9f798f5aa4f3622
daheejo <happyj6528@gmail.com>
2023-08-30 23:57:13 +0900

    feat: MarkerInfoBox 적용, 처음 렌더링 시 사용 가이드 alert

--- src/App.tsx
+++ src/App.tsx
@@ -6,37 +6,44 @@ import Marker from "./components/Marker";
 import DrawingManager from "./components/DrawingManager";
 import { usePositionContext } from "./mapContext";
 import positions from "./constants/positions";
+import MarkerInfoBox from "./components/MarkerInfoBox";
 
 function App() {
   const API_KEY = "AIzaSyBoJnGFrN2OMZfK2ldnfd8aWxDGmz_VnBw";
   const render = (status: Status) => {
     return <h1>{status}</h1>;
   };
-  const { setPositions } = usePositionContext();
+  const { setPositions, selectedPositions } =
+    usePositionContext();
 
   React.useEffect(() => {
     setPositions(positions);
+    window.alert(
+      "지도의 상단 메뉴에서 사각형을 선택하시고 원하는 위치의 마커 위에 그리시면 우측에 위도,경보 정보가 뜹니다 :)"
+    );
   }, []);
 
-  // const [map, setMap] = React.useState<google.maps.Map | null>(null);
-
   return (
-      <div className="app">
-        <Wrapper
-          apiKey={API_KEY}
-          render={render}
-          libraries={["drawing"]}
-          language="ko"
-        >
-          <Map>
-            {positions.map((position, index) => (
-              <Marker key={index} position={position} />
-            ))}
-            <DrawingManager />
-          </Map>
-        </Wrapper>
-        <div className="location-info"></div>
+    <div className="app">
+      <Wrapper
+        apiKey={API_KEY}
+        render={render}
+        libraries={["drawing"]}
+        language="ko"
+      >
+        <Map center={{ lat: 37.569227, lng: 126.9777256 }}>
+          {positions.map((position, index) => (
+            <Marker key={index} position={position} />
+          ))}
+          <DrawingManager />
+        </Map>
+      </Wrapper>
+      <div className="location-info">
+        {selectedPositions?.map((position, index) => (
+          <MarkerInfoBox key={index} info={position} />
+        ))}
       </div>
+    </div>
   );
 }
 
